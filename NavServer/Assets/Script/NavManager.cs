90ea3d8a425e2ea0c3dbe6f5e4bd48c2ae0e5f5b
Arycs <786859650@qq.com>
2022-08-09 21:54:11 +0800

    【服务器】 - 寻路服务器(Unity程序原程序),后续会导出发布到服务器,以供寻路使用

---
+++ NavServer/Assets/Script/NavManager.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavManager : MonoBehaviour
+{
+    /// <summary>
+    /// 寻路代理
+    /// </summary>
+    public NavMeshAgent Agent;
+
+    private NavMeshPath path;
+
+    private LinkedList<ServerClient> m_ServerClientList;
+
+    public string ServerIP = "192.168.0.109";
+    public int ServerPort = 29001;
+
+    public static NavManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        m_ServerClientList = new LinkedList<ServerClient>();
+        path = new NavMeshPath();
+
+        StarListen();
+    }
+    
+    void Update()
+    {
+        LinkedListNode<ServerClient> curr = m_ServerClientList.First;
+        while (curr != null)
+        {
+            curr.Value.OnUpdate();
+            curr = curr.Next;
+        }
+    }
+
+    /// <summary>
+    /// 获取路径点
+    /// </summary>
+    /// <param name="sceneId"></param>
+    /// <param name="beginPos"></param>
+    /// <param name="endPos"></param>
+    /// <returns></returns>
+    public NavMeshPath GetNavPath(int sceneId, Vector3 beginPos, Vector3 endPos)
+    {
+        Agent.Warp(beginPos);
+        Agent.CalculatePath(endPos, path);
+        return path;
+    }
+
+
+    private Socket m_ListenSocket;
+
+    #region StarListen启动监听
+
+    /// <summary>
+    /// 启动监听
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
+    private void StarListen()
+    {
+        //实例化Socket
+        m_ListenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        //向操作系统申请一个可用的IP 和 端口用来通讯
+        m_ListenSocket.Bind(new IPEndPoint(IPAddress.Parse(ServerIP),ServerPort));
+        m_ListenSocket.Listen(20);
+        Debug.LogFormat("寻路服务器 启动监听{0}成功",m_ListenSocket.LocalEndPoint.ToString());
+
+        Thread mThread = new Thread(ListenClientCallBack);
+        mThread.Start();
+    }
+
+    #endregion
+
+    #region ListenClientCallBack 监听回调
+
+    /// <summary>
+    /// 监听回调
+    /// </summary>
+    /// <param name="obj"></param>
+    private void ListenClientCallBack(object obj)
+    {
+        while (true)
+        {
+            //接受服务器客户端请求
+            Socket socket = m_ListenSocket.Accept();
+
+            IPEndPoint point = (IPEndPoint) socket.RemoteEndPoint;
+            Debug.LogFormat("游戏服{0}链接成功",point.ToString());
+            
+            //实例化一个服务器客户端
+            AddServerClient(new ServerClient(socket));
+        }
+    }
+    
+    #endregion
+
+    public void AddServerClient(ServerClient serverClient)
+    {
+        m_ServerClientList.AddLast(serverClient);
+    }
+    
+    public void RemoveServerClient(ServerClient serverClient)
+    {
+        Debug.Log("游戏服断开链接");
+        m_ServerClientList.Remove(serverClient);
+    }
+
+}
---
+++ NavServer/Assets/Script/ServerClient.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.AI;
+using YouYou.Proto;
+using YouYouServer.Core;
+
+/// <summary>
+/// 服务器客户端,
+/// 相当于一个中转站,
+/// GameServer和GateWayServer连接到WorldServer时,WorldServer先创建ServerClient然后根据对应的消息实例化对应的服务器
+/// </summary>
+public class ServerClient
+{
+    /// <summary>
+    /// Socket时间监听派发器
+    /// </summary>
+    public EventDispatcher EventDispatcher { get; private set; }
+
+    /// <summary>
+    /// Socket连接器
+    /// </summary>
+    public ClientSocket ClientSocket { get; private set; }
+
+    public ServerClient(Socket socket)
+    {
+        EventDispatcher = new EventDispatcher();
+
+        ClientSocket = new ClientSocket(socket, EventDispatcher);
+        ClientSocket.OnConnectFail = () => { NavManager.Instance.RemoveServerClient(this); };
+        ClientSocket.OnDisConnect = () => { NavManager.Instance.RemoveServerClient(this); };
+        AddEventListener();
+    }
+
+    public void OnUpdate()
+    {
+        ClientSocket.OnUpdate();
+    }
+
+    /// <summary>
+    /// 监听服务器客户端 连接到服务器
+    /// </summary>
+    private void AddEventListener()
+    {
+        EventDispatcher.AddEventListener(ProtoIdDefine.Proto_GS2NS_GetNavPath, OnGS2NS_GetNavPath);
+    }
+
+    private void OnGS2NS_GetNavPath(byte[] buffer)
+    {
+        GS2NS_GetNavPath proto = GS2NS_GetNavPath.Parser.ParseFrom(buffer);
+
+        NS2GS_ReturnNavPath retProto = new NS2GS_ReturnNavPath();
+
+        NavMeshPath path = NavManager.Instance.GetNavPath(proto.SceneId,
+            new Vector3(proto.BeginPos.X, proto.BeginPos.Y, proto.BeginPos.Z),
+            new Vector3(proto.EndPos.X, proto.EndPos.Y, proto.EndPos.Z));
+
+        retProto.TaskId = proto.TaskId;
+        if (path.status == NavMeshPathStatus.PathComplete)
+        {
+            retProto.Valid = true;
+            Vector3[] vector3s = path.corners;
+            int len = vector3s.Length;
+            for (int i = 0; i < len; i++)
+            {
+                Vector3 vector = vector3s[i];
+                retProto.Path.Add(new NS2GS_Vector3() {X = vector.x, Y = vector.y, Z = vector.z});
+            }
+        }
+        
+        ClientSocket.SendMsg(retProto);
+    }
+}
