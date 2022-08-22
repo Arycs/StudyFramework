13bd322b335c1eb6c31daa1a4afce4d4e6878c83
Arycs <786859650@qq.com>
2022-08-22 16:28:43 +0800

    【服务器】 - 表格补充DBModel与Entity

---
+++ Server/YouYouServer/YouYouServer.Commmon/DataTable/Create/DTSys_ConfigDBModel.cs
@@ -0,0 +1,40 @@
+//===================================================
+//备    注：此代码为工具生成 请勿手工修改
+//===================================================
+using YouYouServer.Common;
+
+namespace YouYouServer.Model.DataTable
+{
+    /// <summary>
+    /// DTSys_Config数据管理
+    /// </summary>
+    public partial class DTSys_ConfigDBModel : DataTableDBModelBase<DTSys_ConfigDBModel, DTSys_ConfigEntity>
+    {
+        /// <summary>
+        /// 数据表完整路径
+        /// </summary>
+        public override string DataTableName => "DTSys_Config";
+
+        /// <summary>
+        /// 加载列表
+        /// </summary>
+        protected override void LoadList(MMO_MemoryStream ms)
+        {
+            int rows = ms.ReadInt();
+            int columns = ms.ReadInt();
+
+            for (int i = 0; i < rows; i++)
+            {
+                DTSys_ConfigEntity entity = new DTSys_ConfigEntity();
+                entity.Id = ms.ReadInt();
+                entity.Desc = ms.ReadUTF8String();
+                entity.Name = ms.ReadUTF8String();
+                entity.Type = ms.ReadUTF8String();
+                entity.Value = ms.ReadUTF8String();
+
+                m_List.Add(entity);
+                m_Dic[entity.Id] = entity;
+            }
+        }
+    }
+}
---
+++ Server/YouYouServer/YouYouServer.Commmon/DataTable/Create/DTSys_ConfigEntity.cs
@@ -0,0 +1,33 @@
+//===================================================
+//备    注：此代码为工具生成 请勿手工修改
+//===================================================
+
+namespace YouYouServer.Model.DataTable
+{
+    /// <summary>
+    /// DTSys_Config实体
+    /// </summary>
+    public partial class DTSys_ConfigEntity : DataTableEntityBase
+    {
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Desc;
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// 字段类型
+        /// </summary>
+        public string Type;
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value;
+
+    }
+}
