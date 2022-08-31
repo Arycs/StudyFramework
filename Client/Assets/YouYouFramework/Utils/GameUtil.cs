85a609a4f1c496810a972041a6a942252186f4da
Arycs <786859650@qq.com>
2022-08-31 12:03:56 +0800

    【工具】 - 修改表格,添加GameUtil辅助类

--- Client/Assets/YouYouFramework/Utils/GameUtil.cs
+++ Client/Assets/YouYouFramework/Utils/GameUtil.cs
@@ -200,4 +200,25 @@ public class GameUtil
         StringHelper.PoolDel(ref sbr);
         return str;
     }
+    
+    
+    /// <summary>
+    /// 计算路径的长度
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static float GetPathLen(Vector3[] path)
+    {
+        float pathLen = 0f; //路径的总长度 计算出路径
+        int len = path.Length;
+        for (int i = 0; i < len; i++)
+        {
+            if (i == len - 1) continue;
+
+            float dis = Vector3.Distance(path[i], path[i + 1]);
+            pathLen += dis;
+        }
+
+        return pathLen;
+    }
 }
---
+++ Server/YouYouServer/YouYouServer.Core/Utils/GameUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace YouYouServer.Core.Utils
+{
+    public class GameUtil
+    {
+        /// <summary>
+        /// 计算路径的长度
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static float GetPathLen(List<UnityEngine.Vector3> path)
+        {
+            float pathLen = 0f; //路径的总长度 计算出路径
+            int len = path.Count;
+            for (int i = 0; i < len; i++)
+            {
+                if (i == len - 1) continue;
+
+                float dis = Vector3.Distance(path[i], path[i + 1]);
+                pathLen += dis;
+            }
+
+            return pathLen;
+        }
+    }
+}
